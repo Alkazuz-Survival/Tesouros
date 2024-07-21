935b90c09b0518e83d49e2d0429b6845bec11253
Alkazuz <kerallis2015@gmail.com>
2024-07-21 12:21:32 -0300

    .

--- src/main/java/br/alkazuz/tesouros/Tesouros.java
+++ src/main/java/br/alkazuz/tesouros/Tesouros.java
@@ -1,24 +1,23 @@
 package br.alkazuz.tesouros;
 
+import br.alkazuz.tesouros.commands.TesourosAdminCommand;
 import br.alkazuz.tesouros.commands.TesourosCommand;
 import br.alkazuz.tesouros.config.ArenasSettings;
 import br.alkazuz.tesouros.config.Settings;
 import br.alkazuz.tesouros.config.manager.ConfigManager;
-import br.alkazuz.tesouros.engines.ArenasTesouroManager;
 import br.alkazuz.tesouros.gui.GuiEditTesouroItems;
+import br.alkazuz.tesouros.gui.GuiShowTesouroItems;
 import br.alkazuz.tesouros.gui.MenuConfirmOpen;
 import br.alkazuz.tesouros.items.TesouroItemManager;
 import br.alkazuz.tesouros.itens.TesouroItems;
 import br.alkazuz.tesouros.listener.PlayerInteractListener;
 import br.alkazuz.tesouros.listener.mcMMOListener;
 import br.alkazuz.tesouros.util.EventWaiter;
+import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.awt.*;
 
 public final class Tesouros extends JavaPlugin {
     private static Tesouros instance;
@@ -27,14 +26,16 @@ public final class Tesouros extends JavaPlugin {
     public void onEnable() {
         instance = this;
         ConfigManager.createConfig("settings");
+        getCommand("tesourosadmin").setExecutor(new TesourosAdminCommand());
         getCommand("tesouros").setExecutor(new TesourosCommand());
         Settings.load();
-        ArenasSettings.load();
         TesouroItems.init();
         loadListeners();
         TesouroItemManager.load();
         eventWaiter = new EventWaiter(this);
         eventWaiter.addEvents(AsyncPlayerChatEvent.class, InventoryClickEvent.class);
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, ArenasSettings::load);
     }
 
     @Override
@@ -49,6 +50,7 @@ public final class Tesouros extends JavaPlugin {
         pm.registerEvents(new PlayerInteractListener(), this);
         pm.registerEvents(new MenuConfirmOpen(), this);
         pm.registerEvents(new GuiEditTesouroItems(), this);
+        pm.registerEvents(new GuiShowTesouroItems(), this);
     }
 
     public static Tesouros getInstance() {
